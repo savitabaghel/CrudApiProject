f6e466d1626994f6c31a2e5731d561fb3b347b7c
Savita Baghel <savitabaghel159@gmail.com>
2022-01-27 11:25:57 +0530

    initial commit

--- src/main/java/com/example/crudproject/CrudprojectApplication.java
+++ src/main/java/com/example/crudproject/CrudprojectApplication.java
@@ -3,9 +3,6 @@ package com.example.crudproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScans;
-//@EntityScan(basePackages = {"com.example.crudproject.service.WalletService", "com.example.crudproject.repository.WalletRepository"})
 
 @SpringBootApplication
 public class CrudprojectApplication {
